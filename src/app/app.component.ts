3530cd5a531a74c8836fc7fdbc98451e4927fb9e
Aleksandr Kurakin <akurakin@technoserv.com>
2017-06-05 18:49:25 +0300

    mon 05-06-17 finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 import {RepositoryService} from "./repository.service";
@@ -10,42 +10,68 @@ import {RepositoryService} from "./repository.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  questForm: FormGroup;
+
   creditPeriods = [
-    {value: '6', viewValue: '6 мес'},
     {value: '12', viewValue: '12 мес'},
     {value: '24', viewValue: '24 мес'},
     {value: '36', viewValue: '36 мес'},
+    {value: '48', viewValue: '48 мес'},
+    {value: '60', viewValue: '60 мес'},
+    {value: '72', viewValue: '72 мес'},
+    {value: '84', viewValue: '84 мес'},
   ];
-  creditPeriod: string;
 
-  creditAmount: FormControl;
-  creditPeriodCtrl: FormControl;
-  stateCtrl: FormControl;
-
-
+  nameCtrl: FormControl;
   filteredStates: any;
 
+  constructor(private fb: FormBuilder, private repositoryService: RepositoryService) {
 
+    console.log(">>> ctor");
+    this.nameCtrl = fb.control('', [Validators.required]);
+    this.questForm = fb.group({
+      creditAmount: ['', [Validators.required, this.numberValidator()]],
+      creditPeriod: ['', [Validators.required]],
+      name: this.nameCtrl,
+      birthDate: ['', [Validators.required]],
+      additionalPhone: ['', [Validators.required, this.phoneValidator()]]
+    });
 
-  constructor(private repositoryService: RepositoryService) {
-
+    this.filteredStates = this.nameCtrl.valueChanges.startWith(null).map(name => this.filterStates(name));
   }
 
   ngOnInit(): void {
-    this.creditAmount = new FormControl();
-    this.creditPeriodCtrl = new FormControl();
-    this.stateCtrl = new FormControl();
-    this.creditPeriodCtrl.registerOnChange(name => this.onPeriodChange(name));
-
-    this.filteredStates = this.stateCtrl.valueChanges.startWith(null).map(name => this.filterStates(name));
-  }
-
-  onPeriodChange(val: string) {
-    console.log(val);
+    console.log(">>> ngOnInit");
   }
 
   filterStates(val: string) {
     return val ? this.repositoryService.getFioSuggestions(val) : null;
   }
 
+  submit(): void {
+    console.log(">>>>>>>>>>>>>>");
+    console.log(this.questForm.value);
+  }
+
+  private numberValidator(): ValidatorFn {
+    const pattern: RegExp = /^[0-9]*$/im;
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!(control.dirty || control.touched)) {
+        return null;
+      } else {
+        return pattern.test(control.value) ? null : {custom: `Invalid phone number`};
+      }
+    };
+  }
+
+  private phoneValidator(): ValidatorFn {
+    const pattern: RegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!(control.dirty || control.touched)) {
+        return null;
+      } else {
+        return pattern.test(control.value) ? null : {custom: `Invalid phone number`};
+      }
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,11 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 
 import {AppComponent} from "./app.component";
-import {MaterialModule, MdDatepickerModule} from "@angular/material";
+import {
+  DateAdapter,
+  MaterialModule, MD_DATE_FORMATS, MD_NATIVE_DATE_FORMATS, MdDatepickerModule,
+  MdNativeDateModule, NativeDateAdapter
+} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpService} from "./http.service";
 import {RepositoryService} from "./repository.service";
@@ -12,6 +16,7 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   imports: [MaterialModule,
+    MdNativeDateModule,
     MdDatepickerModule,
     BrowserAnimationsModule,
     BrowserModule,
@@ -24,6 +29,7 @@ import {FlexLayoutModule} from "@angular/flex-layout";
     AppComponent
   ],
   bootstrap: [AppComponent],
-  providers: [HttpService, RepositoryService]
+  providers: [HttpService, RepositoryService, {provide: DateAdapter, useClass: NativeDateAdapter},
+    {provide: MD_DATE_FORMATS, useValue: MD_NATIVE_DATE_FORMATS}]
 })
 export class AppModule { }
